0f9a8a16cda8f0a5cb36a76a69e7580954b560bf
vish-dawange <vishalsd27@gmail.com>
2012-08-11 16:13:11 +0530

    Program for copying contents of one file to another.

--- copyfile.py
+++ copyfile.py
@@ -6,33 +6,38 @@ print """
 Program:Copying contents of one file to another file
 ======================================================
 """
-#Command line arguements
-script_name, source_file,destination_file =argv
+
 def file_operations() :
-	#Printing the source file and destination file
-	print "Copying from %s to %s "% (source_file ,destination_file)
+	try :
+		#Command line arguements
+		script_name, source_file,destination_file =argv
+		#Printing the source file and destination file
+		print "Copying from %s to %s "% (source_file ,destination_file)
 
-	#Open the source file 
-	input=open(source_file,'r')
+		#Open the source file 
+		input=open(source_file,'r')
 
-	#Read the contnents of file to data
-	data=input.read() 
-	#Size of file contents
-	print "Size of file is %d bytes long" % len(data)
+		#Read the contnents of file to data
+		data=input.read() 
+		#Size of file contents
+		print "Size of file is %d bytes long" % len(data)
 
-	#Checking for existance of file
-	print "Does the output file exist? %r"% exists(destination_file)
-	raw_input() 
+		#Checking for existance of file
+		print "Does the output file exist? %r"% exists(destination_file)
+		raw_input() 
 
-	#Opening the destination file in write mode
-	output=open(destination_file,'w')
-	output.write(data)
+		#Opening the destination file in write mode
+		output=open(destination_file,'w')
+		output.write(data)
 
-	print "Done"
-	#Done with task
+		print "Done"
+		#Done with task
 
-	#Close
-	output.close()
-	input.close()
+		#Close
+		output.close()
+		input.close()
+	except Exception ,err :
+		sys.stderr.write("ERROR---->%s\n"%str(err))
+		
 
 file_operations()
